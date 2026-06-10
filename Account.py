b9076eddb763567628a7ad6779a90652c0b40281
Nexus <timotej.petrik1@gmail.com>
2026-06-10 11:23:24 +0200

    Update Account.py

--- Account.py
+++ Account.py
@@ -1,5 +1,8 @@
 class Account:
-    def __init__(self, name, password):
+    def __init__(self):
+       pass   
+
+   def create_account(self, name, password):
        self.name = name
        self.bal = 1000
        self.password = password
