3510cb35a8dbada9ed901f76d7021233dbadcbcd
Beatle096 <tomas.chrobak2012@gmail.com>
2026-06-12 18:04:15 +0200

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -3,8 +3,14 @@ class Database:
         self.userlist = []
 
     def add_user(self, name, password):
-        user = {"username": name, "password": password}
-        self.list.append(user)
+        user = {"username": name, "password": password, "balance": 1000}
+        self.userlist.append(user)
+
+    def find_user(self, name):
+        for user in self.userlist:
+            if user["username"] == name:
+                return user
+        return None
+
     def read_database(self):
         print(self.userlist)
-        
