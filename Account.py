ae84ff7a48b19fa1ec3610e08ccf51f860e9daa8
Beatle096 <tomas.chrobak2012@gmail.com>
2026-06-13 19:36:31 +0200

    Update Account.py

--- Account.py
+++ Account.py
@@ -1,23 +1,22 @@
-from user-data import *
+from user_data import Database
+
 data = Database()
 
 
 class Account:
     def __init__(self):
-       pass   
+        self.name = None
+        self.password = None
+        self.bal = 0
 
-   def create_account(self, name, password):
-       self.name = name
-       self.bal = 1000
-       self.password = password
-       data.add_user(self.name, self.password)
-    
+    def create_account(self, name, password):
+        self.name = name
+        self.bal = 1000
+        self.password = password
+        data.add_user(self.name, self.password)
+
     def login(self, password):
-        
         if password == self.password:
-   print("LOGGED IN!")
-else:
-    print("Wrong password/username")
-
-
-    
+            return True
+        else:
+            return False
